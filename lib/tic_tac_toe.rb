c6210515695457a4fa7385568a9009dd5d9455a1
Josh Kramer <funemployeddad@gmail.com>
2017-11-29 23:13:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,7 +46,7 @@ end
 
 
 def valid_move?(board, index)
-if index.between?(0, 8) && position_taken?(board, index) == false
+if index.between?(0, 8) && position_taken?(@board, index) == false
 true
 end
 end
